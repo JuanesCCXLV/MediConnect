b2e040797452b248ef8ce111b591eee499574293
AlejoGonzalez255 <alejandro.gonzalez.silva@correounivalle.edu.co>
2025-12-05 22:24:27 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './router'
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router/Router";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
+
+
+
+
+
